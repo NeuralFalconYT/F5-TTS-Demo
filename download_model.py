57d08c7f3372c4c295d787ffb4a478073d0b6aff
Neural Falcon <neuralfalconofficial@gmail.com>
2024-10-13 13:20:22 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -48,7 +48,9 @@ def download_models(base_path, redownload=False):
         ("https://huggingface.co/SWivid/F5-TTS/resolve/main/E2TTS_Base/model_1200000.pt", f"{base_path}/ckpts/E2TTS_Base/model_1200000.pt"),
         ("https://huggingface.co/SWivid/F5-TTS/resolve/main/E2TTS_Base/model_1200000.safetensors", f"{base_path}/ckpts/E2TTS_Base/model_1200000.safetensors"),
         ("https://huggingface.co/SWivid/F5-TTS/resolve/main/F5TTS_Base/model_1200000.pt", f"{base_path}/ckpts/F5TTS_Base/model_1200000.pt"),
-        ("https://huggingface.co/SWivid/F5-TTS/resolve/main/F5TTS_Base/model_1200000.safetensors", f"{base_path}/ckpts/F5TTS_Base/model_1200000.safetensors")
+        ("https://huggingface.co/SWivid/F5-TTS/resolve/main/F5TTS_Base/model_1200000.safetensors", f"{base_path}/ckpts/F5TTS_Base/model_1200000.safetensors"),
+        ("https://huggingface.co/charactr/vocos-mel-24khz/resolve/main/pytorch_model.bin", f"{base_path}/ckpts/vocos-mel-24khz/pytorch_model.bin"),
+        ("https://huggingface.co/charactr/vocos-mel-24khz/resolve/main/config.yaml", f"{base_path}/ckpts/vocos-mel-24khz/config.yaml")
     ]
 
     for url, path in model_urls:
@@ -58,4 +60,4 @@ def download_models(base_path, redownload=False):
 
 base_path = "."
 # base_path = "/content"     
-download_models(base_path, redownload=True)
+download_models(base_path, redownload=False)
